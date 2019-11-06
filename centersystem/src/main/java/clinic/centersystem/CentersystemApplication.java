c1c1f1b93a0f8cab8fcfe2afdb7277eeca4dcc71
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-06 19:21:06 +0100

    Inicijalni commit

---
+++ centersystem/src/main/java/clinic/centersystem/CentersystemApplication.java
@@ -0,0 +1,13 @@
+package clinic.centersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentersystemApplication.class, args);
+	}
+
+}
---
+++ centersystem/src/test/java/clinic/centersystem/CentersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package clinic.centersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
