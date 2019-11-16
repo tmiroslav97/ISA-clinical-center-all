cb43c0cd73d64c0679caf49354c1dde5ca8c7413
Nevena <nvndjukin97@gmail.com>
2019-11-16 19:26:05 +0100

    Forma za registraciju i logovanje

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegPage from './pages/RegPage';
 
 const AppRouter = () =>{
     return(
         <Switch>
             <Route exact path="/" component={HomePage}/>
             <Route exact path="/login" component={LoginPage}/>
+            <Route exact path="/signup" component={RegPage}/>
 
             <Route exact path="/page-not-found" component={() => <h1>Page not found!</h1>}/>
             <Redirect from="*" to="/page-not-found" />
