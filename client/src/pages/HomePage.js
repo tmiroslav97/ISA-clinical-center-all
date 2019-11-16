34b96cdbc62e64c3dbaeb8aa0998f5b5e86adcd8
Nevena <nvndjukin97@gmail.com>
2019-11-16 20:37:57 +0100

    Home page slajder

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { Row, Col, Container} from 'react-bootstrap';
- 
+import { Row, Col, Container, Carousel} from 'react-bootstrap';
+
+
+
 function HomePage() {
     return (
         <Container>
             <Row>
                 <Col md={{ span:10, offset: 1 }} xs={12}>
-                    <div className="text-center">
-                        <h1>Welecome to Clinic Center System!</h1>
-                        <img src="img/home-img.jpg" className="mx-auto d-block" alt="slika"></img>
-                    </div>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="img/home_img.jpg"
+                            alt="First slide"
+                            />
+                            <Carousel.Caption>
+                            <h3>Welecome to Clinic Center System!</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="img/dom_zdravlja.jpg"
+                            alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Our community health centre</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="img/hitna_pomoc.jpg"
+                            alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Our ambulance</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>                
+                    </Carousel>
+                    
                 </Col>
             </Row>
         </Container>
