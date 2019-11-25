71598d95676180de86ee49a2db39a81534284dd6
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-25 18:31:55 +0100

    Refactored form for adding cca

--- client/src/components/ClinicCenterAdmin/AdminReg.js
+++ client/src/components/ClinicCenterAdmin/AdminReg.js
@@ -1,30 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
-function AdminReg(){
-    return(
+function AdminReg() {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [fistName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+
+    const handleRegister = () => {
+
+    };
+
+    return (
         <Container>
             <Row>
-                <Col md={{ span:5, offset: 2 }} xs={12}>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
                     <h2 className="border-bottom">Registrating new admin</h2>
                 </Col>
             </Row>
-                <Row>
-                <Col md={{ span:5, offset: 2 }} xs={12}>
-                        <Form>
-                            <Form.Group >
-                                <Form.Label>E-mail of admin:</Form.Label>
-                                <Form.Control type="text" placeholder="Enter admins e-mail" />
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col}>
+                                <Form.Label>Admin e-mail:</Form.Label>
+                                <Form.Control type="text" placeholder="Enter admin e-mail"
+                                    onChange={({ currentTarget }) => {
+                                        setEmail(currentTarget.value);
+                                    }}
+                                />
                             </Form.Group>
-    
-                            <Form.Group >
-                                <Form.Label>Choose admins password:</Form.Label>
-                                <Form.Control type="password" placeholder="Admin of chosen clinic" />
+                            <Form.Group as={Col}>
+                                <Form.Label>Admin password:</Form.Label>
+                                <Form.Control type="password" placeholder="Enter admin password"
+                                    onChange={({ currentTarget }) => {
+                                        setPassword(currentTarget.value);
+                                    }}
+                                />
                             </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Registrer
-                            </Button>
-                        </Form>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col}>
+                                <Form.Label>First name:</Form.Label>
+                                <Form.Control type="text" placeholder="Enter admin first name"
+                                    onChange={({ currentTarget }) => {
+                                        setFirstName(currentTarget.value);
+                                    }}
+                                />
+                            </Form.Group>
+                            <Form.Group as={Col}>
+                                <Form.Label>Last name:</Form.Label>
+                                <Form.Control type="text" placeholder="Enter admin last name"
+                                    onChange={({ currentTarget }) => {
+                                        setLastName(currentTarget.value);
+                                    }}
+                                />
+                            </Form.Group>
+                        </Form.Row>
+                        <Button variant="primary" onClick={handleRegister}>
+                            Register
+                        </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
