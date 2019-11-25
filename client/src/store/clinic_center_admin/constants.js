ff9af6dceef4440af23b446c345c7ac9b1fdc72d
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-25 18:34:31 +0100

    Added constants and action for registering cca

--- client/src/store/clinic_center_admin/constants.js
+++ client/src/store/clinic_center_admin/constants.js
@@ -3,4 +3,5 @@ export const PUT_CCADMIN_DATA = 'put_ccadmin_data';
 export const FETCH_REG_REQS_DATA = 'fetch_reg_reqs_data';
 export const PUT_REG_REQS_DATA = 'put_req_reqs_data';
 export const APPROVE_REG_REQ = 'approve_reg_req';
-export const REJECT_REG_REQ = 'reject_reg_req';
+export const REJECT_REG_REQ = 'reject_reg_req'; 
+export const REG_CC_ADMIN = 'register_cc_admin';
