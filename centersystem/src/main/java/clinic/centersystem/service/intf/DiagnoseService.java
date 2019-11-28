1f8c2bc748d47a7fa0b979bc9bb669ca7c9862f7
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-28 08:03:04 +0100

    Diagnose service added

---
+++ centersystem/src/main/java/clinic/centersystem/service/intf/DiagnoseService.java
@@ -0,0 +1,15 @@
+package clinic.centersystem.service.intf;
+
+import clinic.centersystem.model.Diagnose;
+
+import java.util.List;
+
+public interface DiagnoseService {
+
+    Diagnose findById(Long id);
+
+    List<Diagnose> findAll();
+
+    Diagnose save();
+
+}
