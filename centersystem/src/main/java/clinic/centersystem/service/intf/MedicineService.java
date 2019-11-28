ebf891012c17d5ed6bd6f8718e352c21716fea46
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-28 21:27:29 +0100

    Adding medicine service

---
+++ centersystem/src/main/java/clinic/centersystem/service/intf/MedicineService.java
@@ -0,0 +1,15 @@
+package clinic.centersystem.service.intf;
+
+import clinic.centersystem.dto.request.MedicineRequestDTO;
+import clinic.centersystem.model.Medicine;
+
+import java.util.List;
+
+public interface MedicineService {
+
+    Medicine findById(Long id);
+
+    List<Medicine> findAll();
+
+    Medicine save(MedicineRequestDTO medicineRequestDTO);
+}
