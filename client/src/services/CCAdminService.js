9bf7219ab607229d12d421942f588a0d08c54dc2
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-30 23:57:44 +0100

    Added frontend service for post method for adding diagnose

--- client/src/services/CCAdminService.js
+++ client/src/services/CCAdminService.js
@@ -11,7 +11,8 @@ const FINALPOINTS = {
     REG_CLINIC: '/cca/reg-clinic',
     FETCH_CLINICS: '/cca/clinics',
     REG_CLINIC_ADMIN: '/cca/reg-clinic-admin',
-    ADD_MEDICINE : '/cca/add-medicine'
+    ADD_MEDICINE : '/cca/add-medicine',
+    ADD_DIAGNOSE : '/cca/add-diagnose'
 };
 
 class CCAdminService extends HttpClient {
@@ -119,6 +120,19 @@ class CCAdminService extends HttpClient {
         try {
             const { data } = await this.getApiClient().post(
                 FINALPOINTS.ADD_MEDICINE,
+                payload
+            );
+
+            return { data };
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
+    addDiagnose = async payload => {
+        try {
+            const { data } = await this.getApiClient().post(
+                FINALPOINTS.ADD_DIAGNOSE,
                 payload
             );
 
