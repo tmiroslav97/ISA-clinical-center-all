d5326839f065c71156451bf6faa1a3eb1e4fdc18
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-11-30 23:31:42 +0100

    Added view for adding medicine

--- client/src/components/ClinicCenterAdmin/AddMedicine.js
+++ client/src/components/ClinicCenterAdmin/AddMedicine.js
@@ -1,21 +1,83 @@
-import React from 'react';
-import { Container, Row, Spinner } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 
-function AddMedicine(){
-    return(
+
+function AddMedicine() {
+    const dispatch = useDispatch();
+    const [name, setName] = useState();
+    const [description, setDescription] = useState();
+    const [code, setCode] = useState();
+
+    const handleAddMedicine = () =>{
+
+    };
+
+    return (
         <Container>
             <Row>
-                <h3>Adding medicines - in progress</h3>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <h2 className="border-bottom">Add medicine</h2>
+                </Col>
             </Row>
             <Row>
-                <Spinner animation="grow" variant="primary" />
-                <Spinner animation="grow" variant="secondary" />
-                <Spinner animation="grow" variant="success" />
-                <Spinner animation="grow" variant="danger" />
-                <Spinner animation="grow" variant="warning" />
-                <Spinner animation="grow" variant="info" />
-                <Spinner animation="grow" variant="light" />
-                <Spinner animation="grow" variant="dark" />
+                <Col md={{ span: 5, offset: 1 }} xs={12}>
+                    <Form>
+                        <Form.Group as={Col}>
+                            <Form.Label>Code:</Form.Label>
+                            <Form.Control type="text" placeholder="Enter medicine code"
+                                onChange={({ currentTarget }) => {
+                                    setCode(currentTarget.value);
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Label>Name::</Form.Label>
+                            <Form.Control type="text" placeholder="Enter medicine name"
+                                onChange={({ currentTarget }) => {
+                                    setName(currentTarget.value);
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Label>Description address:</Form.Label>
+                            <Form.Control type="textarea" placeholder="Enter medicine description"
+                                onChange={({ currentTarget }) => {
+                                    setDescription(currentTarget.value);
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Button variant="primary" onClick={handleAddMedicine}>
+                                Register
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <h3>Clinics list</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <Table responsive>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Code</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                
+                            }
+                        </tbody>
+                    </Table>
+                </Col>
             </Row>
         </Container>
     );
