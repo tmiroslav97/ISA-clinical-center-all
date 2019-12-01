93e846e0bf08fe900dfd1a75cd8541846bc93b60
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-01 17:01:35 +0100

    Changes for table names

--- centersystem/src/main/java/clinic/centersystem/common/db/DbTableConstants.java
+++ centersystem/src/main/java/clinic/centersystem/common/db/DbTableConstants.java
@@ -16,12 +16,12 @@ public final class DbTableConstants {
     public static final String DIAGNOSE = "diagnose";
     public static final String DIAGNOSERECORD = "diagnoseRecord";
     public static final String DOCTOR = "doctor";
-    public static final String MEDECINE = "medecine";
+    public static final String MEDICINE = "medicine";
     public static final String MEDICALRECORD = "medicalRecord";
     public static final String MEDICALREPORT = "medicalReport";
     public static final String NURSE = "nurse";
     public static final String PATIENT = "patient";
-    public static final String PERSCRIPTIONRECORD = "perscriptionRecord";
+    public static final String MEDICINERECORD = "medicineRecord";
     public static final String PERSONNEL = "personnel";
     public static final String PRICELIST = "priceList";
     public static final String PRICELISTITEM = "priceListItem";
