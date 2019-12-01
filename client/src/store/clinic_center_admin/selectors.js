a712e9ea87e36de3080a829e1e782344bf73f063
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-01 12:11:12 +0100

    Added selector for medicines data on frontend

--- client/src/store/clinic_center_admin/selectors.js
+++ client/src/store/clinic_center_admin/selectors.js
@@ -3,3 +3,4 @@ const reducer = 'ccAdminReducer';
 export const ccAdminDataSelector = state => state[reducer].data;
 export const regReqsDataSelector = state => state[reducer].reqs;
 export const clinicsDataSelector = state => state[reducer].clinics;
+export const medicinesDataSelector = state => state[reducer].medicines;
