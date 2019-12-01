8c6c6faa0cd4f6414c5ab29d2b4f73c28231259c
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-01 12:01:33 +0100

    Added medicine service for business logic

---
+++ centersystem/src/main/java/clinic/centersystem/service/MedicineServiceCont.java
@@ -0,0 +1,21 @@
+package clinic.centersystem.service;
+
+import clinic.centersystem.model.Medicine;
+import clinic.centersystem.service.intf.MedicineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MedicineServiceCont {
+
+    @Autowired
+    private MedicineService medicineService;
+
+    public List<Medicine> getMedicines() {
+        return medicineService.findAll();
+    }
+}
