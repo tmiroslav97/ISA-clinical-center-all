7a25041ccabedb39c8e7465c1d4330ef07b33be1
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-02 23:33:34 +0100

    Added constants

--- client/src/store/nurse/constants.js
+++ client/src/store/nurse/constants.js
@@ -1,2 +1,4 @@
 export const FETCH_NURSE_DATA = 'fetch_nurse_data';
+export const PUT_NURSE_DATA = 'put_nurse_data';
 export const FETCH_PATIENTS = 'fetch_patients';
+export const PUT_PATIENTS = 'put_patients';
