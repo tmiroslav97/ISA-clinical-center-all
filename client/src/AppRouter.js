2d8a7ff0d36ca48aa5007b49dee541ea99cccff5
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-02 22:16:36 +0100

    Added route for nurse home page in app router

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import {Route, Redirect, Switch} from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import PatientHomePage from './components/PatientHomePage';
 import RegPage from './pages/RegPage';
 import DoctorHomePage from './pages/DoctorHomePage';
+import NurseHomePage from './pages/NurseHomePage';
 import ClinicCenterAdminProfile from './components/ClinicCenterAdmin/ClinicCenterAdminProfile';
 import HolidayAproval from './components/HolidayAproval';
 import PasswordChanger from './components/PasswordChanger';
-const AppRouter = () =>{
-    return(
+const AppRouter = () => {
+    return (
         <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route exact path="/login" component={LoginPage}/>
-            <Route exact path="/pat" component={PatientHomePage}/>
-            <Route exact path="/signup" component={RegPage}/>
-            <Route exact path="/doc" component={DoctorHomePage}/>
-            <Route exact path="/prob" component={HolidayAproval}/>
-            <Route exact path="/ccadmin/:id" component={ClinicCenterAdminProfile}/>
-            <Route exact path="/signup" component={RegPage}/>
-            <Route exact path="/change-pass" component={PasswordChanger}/>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/pat" component={PatientHomePage} />
+            <Route exact path="/signup" component={RegPage} />
+            <Route exact path="/doc" component={DoctorHomePage} />
+            <Route exact path="/nurse-page" component={NurseHomePage} />
+            <Route exact path="/prob" component={HolidayAproval} />
+            <Route exact path="/ccadmin/:id" component={ClinicCenterAdminProfile} />
+            <Route exact path="/signup" component={RegPage} />
+            <Route exact path="/change-pass" component={PasswordChanger} />
 
-            <Route exact path="/page-not-found" component={() => <h1>Page not found!</h1>}/>
+            <Route exact path="/page-not-found" component={() => <h1>Page not found!</h1>} />
             <Redirect from="*" to="/page-not-found" />
         </Switch>
     );
