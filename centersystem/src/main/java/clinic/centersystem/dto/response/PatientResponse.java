b7b6c3ba4424044a6cbce66142506b7be9d2e7f0
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-02 21:21:30 +0100

    Added patient dto for response

---
+++ centersystem/src/main/java/clinic/centersystem/dto/response/PatientResponse.java
@@ -0,0 +1,27 @@
+package clinic.centersystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientResponse {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String address;
+
+    private String country;
+
+    private String city;
+
+    private String unoip;
+
+    private String phoneNum;
+}
