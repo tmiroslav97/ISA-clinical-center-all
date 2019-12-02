7b4d9091497f3bc18d545c9e548c4bb97125336f
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-02 23:35:08 +0100

    Added put actions

--- client/src/store/nurse/actions.js
+++ client/src/store/nurse/actions.js
@@ -1,6 +1,8 @@
 import {
     FETCH_NURSE_DATA,
-    FETCH_PATIENTS
+    PUT_NURSE_DATA,
+    FETCH_PATIENTS,
+    PUT_PATIENTS
 } from './constants';
 
 export const fetchNurseData = payload => ({
@@ -8,7 +10,17 @@ export const fetchNurseData = payload => ({
     payload
 });
 
+export const putNurseData = payload => ({
+    type: PUT_NURSE_DATA,
+    payload
+});
+
 export const fetchPatients = payload => ({
     type: FETCH_PATIENTS,
+    payload
+});
+
+export const putPatients = payload => ({
+    type: PUT_PATIENTS,
     payload
 });
