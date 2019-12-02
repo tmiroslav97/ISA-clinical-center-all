068d08636625610c8e96a0e307c6e2099b9cdf8c
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-02 23:17:33 +0100

    Added reducer and initial state for nurse

---
+++ client/src/store/nurse/reducer/index.js
@@ -0,0 +1,23 @@
+const initialState = {
+    data: {
+        id: window.localStorage.getItem('id'),
+        firstName: '',
+        lastName: '',
+        email: '',
+        role: window.localStorage.getItem('role'),
+    },
+    patients: []
+};
+
+const nurseReducer = (state = initialState, { type, payload }) => {
+    if (actionHandler.hasOwnProperty(type)) {
+        return actionHandler[type](state, payload);
+    }
+    return state;
+};
+
+const actionHandler = {
+
+};
+
+export default nurseReducer;
