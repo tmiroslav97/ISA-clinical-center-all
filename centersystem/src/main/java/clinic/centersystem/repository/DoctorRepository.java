7c5f6bcfcefcce6364b95de810eda1f7f33a6681
Nevena <nvndjukin97@gmail.com>
2019-12-02 23:31:52 +0100

    Changed doctors model, added constructor

--- centersystem/src/main/java/clinic/centersystem/repository/DoctorRepository.java
+++ centersystem/src/main/java/clinic/centersystem/repository/DoctorRepository.java
@@ -5,5 +5,6 @@ import clinic.centersystem.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-    Doctor findByEmail(String email);
+   // Doctor findById(Long id);
+
 }
