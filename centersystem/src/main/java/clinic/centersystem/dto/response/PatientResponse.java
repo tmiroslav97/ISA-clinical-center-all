1cb43027a2e25fb9ce7a7c567706fb01768942fa
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-03 01:38:18 +0100

    Added missing data annotation

--- centersystem/src/main/java/clinic/centersystem/dto/response/PatientResponse.java
+++ centersystem/src/main/java/clinic/centersystem/dto/response/PatientResponse.java
@@ -2,8 +2,10 @@ package clinic.centersystem.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
