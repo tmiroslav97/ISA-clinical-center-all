87c8c6bb9cd89f02887d9ffb2989030a0bfe3b33
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-03 19:32:14 +0100

    Added nurse response class

---
+++ centersystem/src/main/java/clinic/centersystem/dto/response/NurseResponse.java
@@ -0,0 +1,21 @@
+package clinic.centersystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NurseResponse {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
