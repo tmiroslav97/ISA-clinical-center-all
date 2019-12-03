3ec9012a16d02f5a25c1ca66949e3e7b0727c377
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 00:28:37 +0100

    Added absence req converter

---
+++ centersystem/src/main/java/clinic/centersystem/converter/AbsenceRequirementConverter.java
@@ -0,0 +1,15 @@
+package clinic.centersystem.converter;
+
+import clinic.centersystem.dto.request.AbsenceRequirementDTO;
+import clinic.centersystem.model.AbsenceRequirement;
+
+public class AbsenceRequirementConverter {
+
+    public static AbsenceRequirement toCreateAbsenceRequirementFromAbsenceRequest(AbsenceRequirementDTO absenceRequirementDTO) {
+        return AbsenceRequirement.builder()
+                .type(absenceRequirementDTO.getType())
+                .startDate(absenceRequirementDTO.getStartDate())
+                .endDate(absenceRequirementDTO.getEndDate())
+                .build();
+    }
+}
