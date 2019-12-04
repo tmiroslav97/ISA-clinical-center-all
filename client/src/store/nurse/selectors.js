62545d00fc3e7539b0e6cd547b670d7b48384d69
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 17:15:26 +0100

    Added data selector for recepies

--- client/src/store/nurse/selectors.js
+++ client/src/store/nurse/selectors.js
@@ -3,3 +3,4 @@ const reducer = 'nurseReducer';
 export const nurseDataSelector = state => state[reducer].data;
 export const patientsSelector = state => state[reducer].patients;
 export const absHolRequestDataSelector = state => state[reducer].absholrequests;
+export const recepiesDataSelector = state => state[reducer].recepies;
