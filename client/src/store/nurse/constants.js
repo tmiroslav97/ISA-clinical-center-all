5a5258a8ff266c3b11e75d6fe3d3041765b3cd71
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 17:05:08 +0100

    Added constants for rewriting recepies

--- client/src/store/nurse/constants.js
+++ client/src/store/nurse/constants.js
@@ -5,3 +5,6 @@ export const PUT_PATIENTS = 'put_patients';
 export const ABS_HOL_REQUEST = 'abs_hol_request';
 export const PUT_ABS_HOL_REQUEST = 'put_abs_hol_request';
 export const FETCH_ABS_HOL_REQUEST = 'fetch_abs_hol_request';
+export const FETCH_RECEPIES = 'fetch_recepies';
+export const PUT_RECEPIES_DATA = 'put_recepies_data';
+export const REWRITE_RECEPIE = 'rewrite_recepie';
