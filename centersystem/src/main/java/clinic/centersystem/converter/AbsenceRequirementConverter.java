68fcfac848938982ce65d5eaf0bdfe32824f4727
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 14:17:35 +0100

    Added converter for absence requirement response

--- centersystem/src/main/java/clinic/centersystem/converter/AbsenceRequirementConverter.java
+++ centersystem/src/main/java/clinic/centersystem/converter/AbsenceRequirementConverter.java
@@ -1,6 +1,7 @@
 package clinic.centersystem.converter;
 
 import clinic.centersystem.dto.request.AbsenceRequirementDTO;
+import clinic.centersystem.dto.response.AbsenceRequirementResponse;
 import clinic.centersystem.model.AbsenceRequirement;
 
 public class AbsenceRequirementConverter {
@@ -11,6 +12,15 @@ public class AbsenceRequirementConverter {
                 .startDate(absenceRequirementDTO.getStartDate())
                 .endDate(absenceRequirementDTO.getEndDate())
                 .status("REQUESTED")
+                .build();
+    }
+
+    public static AbsenceRequirementResponse toCreateAbsenceRequirementResponseFromAbsenceRequirement(AbsenceRequirement absenceRequirement) {
+        return AbsenceRequirementResponse.builder()
+                .type(absenceRequirement.getType())
+                .status(absenceRequirement.getStatus())
+                .startDate(absenceRequirement.getStartDate())
+                .endDate(absenceRequirement.getEndDate())
                 .build();
     }
 }
