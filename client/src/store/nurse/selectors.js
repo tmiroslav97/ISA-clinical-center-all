6e93400da6af919f7e247300cbe9a6e9093e35d5
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 14:31:25 +0100

    Added selector for absence holiday requirement

--- client/src/store/nurse/selectors.js
+++ client/src/store/nurse/selectors.js
@@ -2,3 +2,4 @@ const reducer = 'nurseReducer';
 
 export const nurseDataSelector = state => state[reducer].data;
 export const patientsSelector = state => state[reducer].patients;
+export const absHolRequestDataSelector = state => state[reducer].absholrequests;
