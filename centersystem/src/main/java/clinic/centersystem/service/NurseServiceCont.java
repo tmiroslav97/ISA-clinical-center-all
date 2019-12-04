f1b37600911c40372a57b9071c48411c64b65d16
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 16:15:40 +0100

    Added method for rewrite perscription in nurse service controller

--- centersystem/src/main/java/clinic/centersystem/service/NurseServiceCont.java
+++ centersystem/src/main/java/clinic/centersystem/service/NurseServiceCont.java
@@ -4,6 +4,8 @@ package clinic.centersystem.service;
 import clinic.centersystem.converter.NurseConverter;
 import clinic.centersystem.dto.response.NurseResponse;
 import clinic.centersystem.model.Nurse;
+import clinic.centersystem.model.Recepie;
+import clinic.centersystem.service.intf.RecepieService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,27 @@ public class NurseServiceCont {
     @Autowired
     private NurseServiceImpl nurseService;
 
+    @Autowired
+    private RecepieService recepieService;
+
     public NurseResponse getNurseById(Long id) {
         Nurse nurse = this.nurseService.findById(id);
         NurseResponse nurseResponse = NurseConverter.toCreateNurseResponseFromNurse(nurse);
         return nurseResponse;
+    }
+
+    public String rewriteRecepie(Long recepieId, Long nurseId) {
+        Nurse nurse = nurseService.findById(nurseId);
+        Recepie recepie = recepieService.findById(recepieId);
+
+        recepie.setValidate(true);
+        nurse.getRecepies().add(recepie);
+        recepie.setNurse(nurse);
+
+        nurse = nurseService.save(nurse);
+        recepie = recepieService.save(recepie);
+
+        return "Successfullt rewrite recepie";
     }
 
 }
