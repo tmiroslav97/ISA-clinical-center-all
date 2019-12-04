169811fdd6b47c1ff175e1a9d237a7c1ef5184ca
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 12:14:53 +0100

    Added constants for actions

--- client/src/store/nurse/constants.js
+++ client/src/store/nurse/constants.js
@@ -2,3 +2,5 @@ export const FETCH_NURSE_DATA = 'fetch_nurse_data';
 export const PUT_NURSE_DATA = 'put_nurse_data';
 export const FETCH_PATIENTS = 'fetch_patients';
 export const PUT_PATIENTS = 'put_patients';
+export const ABS_HOL_REQUEST = 'abs_hol_request';
+export const PUT_ABS_HOL_REQUEST = 'put_abs_hol_request';
