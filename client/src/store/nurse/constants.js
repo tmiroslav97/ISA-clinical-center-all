9490535e0bcbba0c5007a6e2db31239892fc0f1c
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 14:27:22 +0100

    Added constant for fetching nurse abs hol requirement

--- client/src/store/nurse/constants.js
+++ client/src/store/nurse/constants.js
@@ -4,3 +4,4 @@ export const FETCH_PATIENTS = 'fetch_patients';
 export const PUT_PATIENTS = 'put_patients';
 export const ABS_HOL_REQUEST = 'abs_hol_request';
 export const PUT_ABS_HOL_REQUEST = 'put_abs_hol_request';
+export const FETCH_ABS_HOL_REQUEST = 'fetch_abs_hol_request';
