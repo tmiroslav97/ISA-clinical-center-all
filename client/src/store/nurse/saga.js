503ab7cd9a51f2d655359cc2d1aef6d916f4ea4d
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 17:21:31 +0100

    Added functions in saga for recepies

--- client/src/store/nurse/saga.js
+++ client/src/store/nurse/saga.js
@@ -4,7 +4,9 @@ import {
     FETCH_NURSE_DATA,
     FETCH_PATIENTS,
     ABS_HOL_REQUEST,
-    FETCH_ABS_HOL_REQUEST
+    FETCH_ABS_HOL_REQUEST,
+    FETCH_RECEPIES,
+    REWRITE_RECEPIE
 } from './constants';
 
 import PatientService from '../../services/PatientService';
@@ -14,8 +16,23 @@ import PersonnelService from '../../services/PersonnelService';
 import {
     putNurseData,
     putPatients,
-    putAbsHolRequest
+    putAbsHolRequest,
+    putRecepiesData
 } from './actions';
+
+export function* rewriteRecepie() {
+    const { payload } = yield take(REWRITE_RECEPIE);
+    const { data } = yield call(NurseService.rewriteRecepie, payload);
+    const { recepies } = yield call(NurseService.fetchRecepies, {});
+    yield put(putRecepiesData(recepies));
+}
+
+
+export function* fetchRecepies() {
+    const { payload } = yield take(FETCH_RECEPIES);
+    const { recepies } = yield call(NurseService.fetchRecepies, payload);
+    yield put(putRecepiesData(recepies));
+}
 
 export function* fetchAbsHolRequest() {
     const { payload } = yield take(FETCH_ABS_HOL_REQUEST);
