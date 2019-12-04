5c35db2021673981037865cd2b43a9105c06e9fc
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 16:34:15 +0100

    Added recepie response DTO

---
+++ centersystem/src/main/java/clinic/centersystem/dto/response/RecepieResponse.java
@@ -0,0 +1,15 @@
+package clinic.centersystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecepieResponse {
+    
+    private Long id;
+}
