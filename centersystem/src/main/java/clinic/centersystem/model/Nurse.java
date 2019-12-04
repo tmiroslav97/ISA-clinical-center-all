cf126a2016f5c2de058b867298ae4e7ad7cd700f
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-04 16:04:34 +0100

    Added missing field for recepies in nurse class

--- centersystem/src/main/java/clinic/centersystem/model/Nurse.java
+++ centersystem/src/main/java/clinic/centersystem/model/Nurse.java
@@ -4,15 +4,16 @@ package clinic.centersystem.model;
 import clinic.centersystem.common.db.DbColumnConstants;
 import clinic.centersystem.common.db.DbTableConstants;
 import clinic.centersystem.model.enumeration.RoleEnum;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -23,12 +24,16 @@ import java.util.Set;
 @Table(name = DbTableConstants.NURSE)
 public class Nurse extends Personnel {
 
+    @JsonBackReference
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Recepie> recepies = new HashSet<Recepie>();
+
     @Builder(builderMethodName = "nurseBuilder")
     public Nurse(Long id, String email, String password, String firstName, String lastName,
                  boolean enabled, RoleEnum role, boolean isFirstLog, Timestamp lastPasswordResetDate,
                  List<Authority> authorities, Set<Patient> patients, Clinic clinic, Calendar calendar,
-                 Set<AbsenceRequirement> absenceRequirements, Set<Appointment> appointments) {
+                 Set<AbsenceRequirement> absenceRequirements, Set<Appointment> appointments, Set<Recepie> recepies) {
         super(id, email, password, firstName, lastName, enabled, role, isFirstLog, lastPasswordResetDate, authorities, patients, clinic, calendar, absenceRequirements, appointments);
-
+        this.recepies = recepies;
     }
 }
