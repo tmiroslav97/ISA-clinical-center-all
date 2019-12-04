aa0da336645cac73a221c6701765267b389e8c5d
Nevena <nvndjukin97@gmail.com>
2019-12-04 15:33:04 +0100

    Added modal for adding doctor

--- client/src/components/ClinicAdmin/DoctorAllAtOnce.js
+++ client/src/components/ClinicAdmin/DoctorAllAtOnce.js
@@ -1,9 +1,79 @@
-import React from 'react';
-import { Container, Row, Form, Col, Button, Table } from 'react-bootstrap';
+import React,{useState} from 'react';
+import { Container, Row, Form, Col, Button, Table, Modal } from 'react-bootstrap';
 
 
 const DoctorAllAtOnce = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
+        <>
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add data:</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} >
+                            <Form.Label>E-mail address</Form.Label>
+                            <Form.Control type="email" placeholder="E-mail" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="City" />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group as={Col} >
+                            <Form.Label>Confirm password</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm your password" />
+                        </Form.Group>
+
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} >
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type="text" placeholder="Address" />
+                        </Form.Group>
+                        <Form.Group as={Col} >
+                            <Form.Label>Country</Form.Label>
+                            <Form.Control type="text" placeholder="Country" />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control type="text" placeholder="First name" />
+                        </Form.Group>
+                        <Form.Group as={Col} >
+                            <Form.Label>Contact</Form.Label>
+                            <Form.Control type="text" placeholder="Contact" />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} >
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control type="text" placeholder="Last name" />
+                        </Form.Group>
+
+                    </Form.Row>
+                    
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={handleClose}>
+                    Add
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+
         <Container>
             <Row>
                 <h3>Doctors - adding, deliting and searching</h3>
@@ -16,7 +86,7 @@ const DoctorAllAtOnce = () => {
 
                         <Form.Label>Add new doctor:</Form.Label>
                         <Col>
-                            <Button>Add </Button>
+                            <Button onClick={handleShow}>Add </Button>
                         </Col>
                     </Form.Group>
 
@@ -60,8 +130,9 @@ const DoctorAllAtOnce = () => {
                 </Table>
             </Row>
 
-            
+
         </Container >
+        </>
     );
 }
 
