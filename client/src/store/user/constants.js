f6f0ca61c7eaefe9ada51194da0bb9a6f555a976
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-07 00:42:30 +0100

    Added constant for sign out

--- client/src/store/user/constants.js
+++ client/src/store/user/constants.js
@@ -3,3 +3,4 @@ export const REGISTRATION = 'registration';
 export const PUT_USER_DATA = 'put_user_data';
 export const PUT_USER_TOKEN = 'put_user_token';
 export const CHANGE_PASSWORD = 'change_password';
+export const SIGN_OUT = 'sign_out';
