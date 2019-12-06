a94caa61655a5f53e710d8d7bf8a2f4894f5e445
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-06 16:39:24 +0100

    Refactoring to private from public api for changing password

--- centersystem/src/main/java/clinic/centersystem/config/WebSecurityConfig.java
+++ centersystem/src/main/java/clinic/centersystem/config/WebSecurityConfig.java
@@ -71,7 +71,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers(HttpMethod.POST, "/sec/login");
         web.ignoring().antMatchers(HttpMethod.POST, "/sec/registration");
-        web.ignoring().antMatchers(HttpMethod.POST, "/sec/change-password");
         web.ignoring().antMatchers(HttpMethod.GET, "/cca/activate-account/*");
         web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",
                 "/**/*.css", "/**/*.js", "/**/assets/**");
