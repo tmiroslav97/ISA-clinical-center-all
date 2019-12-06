aa91148681fcbd8781c967926f0f4446c09814ae
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-07 00:51:03 +0100

    Connected model with view (nedded data for showing login,sign up or sign out

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import { Navbar, Nav, } from 'react-bootstrap';
 import { userDataSelector } from '../store/user/selectors';
 import { useSelector } from 'react-redux';
+import { signOut } from '../store/user/actions';
 
 export default function NavBar() {
+    const dispatch = useDispatch();
     const user = useSelector(userDataSelector);
     const role = user.role;
+
+    const handleSignOut = () => {
+        dispatch(
+            signOut({
+                'id': '',
+                'firstName': '',
+                'lastName': '',
+                'email': '',
+                'firstLog': '',
+                'role': 'ROLE_USER'
+            })
+        );
+    };
+
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/">Clinic Center</Navbar.Brand>
@@ -15,9 +32,9 @@ export default function NavBar() {
                     <Nav.Link href="/">Home</Nav.Link>
                 </Nav>
                 <Nav className="ml-auto">
-                    {role==='ROLE_USER' && <Nav.Link href="/login">Login</Nav.Link>}
-                    {role==='ROLE_USER' && <Nav.Link href="/signup">Sign Up</Nav.Link>}
-                    {role!=='ROLE_USER' && <Nav.Link href="/signup">Sign out</Nav.Link>}
+                    {role === 'ROLE_USER' && <Nav.Link href="/login">Login</Nav.Link>}
+                    {role === 'ROLE_USER' && <Nav.Link href="/signup">Sign Up</Nav.Link>}
+                    {role !== 'ROLE_USER' && <Nav.Link href="#" onClick={() => handleSignOut()}>Sign out</Nav.Link>}
                 </Nav>
 
             </Navbar.Collapse>
