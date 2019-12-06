c9e2b76141955ed3846fcd2380b572a3a263c1ff
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-07 00:36:56 +0100

    Added logic for showing login, signup or signout depending of logged user

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Navbar, Nav, } from 'react-bootstrap';
-import {userDataSelector} from '../store/user/selectors';
-import {useSelector} from 'react-redux';
+import { userDataSelector } from '../store/user/selectors';
+import { useSelector } from 'react-redux';
 
 export default function NavBar() {
-    // esling-disable-next-line
     const user = useSelector(userDataSelector);
+    const role = user.role;
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/">Clinic Center</Navbar.Brand>
@@ -14,16 +14,15 @@ export default function NavBar() {
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
                 </Nav>
-                
                 <Nav className="ml-auto">
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/signup">Sign Up</Nav.Link>
+                    {role==='ROLE_USER' && <Nav.Link href="/login">Login</Nav.Link>}
+                    {role==='ROLE_USER' && <Nav.Link href="/signup">Sign Up</Nav.Link>}
+                    {role!=='ROLE_USER' && <Nav.Link href="/signup">Sign out</Nav.Link>}
                 </Nav>
-                
-                
+
             </Navbar.Collapse>
         </Navbar>
 
     );
-  }
- 
+}
+
