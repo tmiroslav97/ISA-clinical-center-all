fd02856c69e713800305fff64388337b15276c48
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-06 16:30:03 +0100

    Changed model for password changer and moved into DTO classes, other changes are affected by this change

--- centersystem/src/main/java/clinic/centersystem/controller/AuthenticationController.java
+++ centersystem/src/main/java/clinic/centersystem/controller/AuthenticationController.java
@@ -3,17 +3,14 @@ package clinic.centersystem.controller;
 import clinic.centersystem.authentication.JwtAuthenticationRequest;
 import clinic.centersystem.dto.request.RegistrationRequirementDTO;
 import clinic.centersystem.dto.response.LoginUserResponse;
-import clinic.centersystem.model.PasswordChanger;
+import clinic.centersystem.dto.request.PasswordChangerRequestDTO;
 import clinic.centersystem.service.AuthenticationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/sec", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -42,8 +39,8 @@ public class AuthenticationController {
     }
 
     @RequestMapping(value = "/change-password", method = RequestMethod.POST)
-    public ResponseEntity<?> changePassword(@RequestBody PasswordChanger passwordChanger) {
-        boolean flag = authenticationService.changePassword(passwordChanger);
+    public ResponseEntity<?> changePassword(@RequestBody PasswordChangerRequestDTO passwordChangerRequestDTO) {
+        boolean flag = authenticationService.changePassword(passwordChangerRequestDTO);
         if (flag) {
             return new ResponseEntity<>("Successfully changed password!", HttpStatus.OK);
         } else {
