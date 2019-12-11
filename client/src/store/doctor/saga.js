a292423792fd58bdb35829c479839301996f2965
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-11 13:42:35 +0100

    Added saga functions for fethcing doctor data

---
+++ client/src/store/doctor/saga.js
@@ -0,0 +1,22 @@
+import { take, put, call } from 'redux-saga/effects';
+
+import {
+    FETCH_DOCTOR_DATA
+} from './constants';
+
+import {
+    putDoctorData
+} from './actions';
+
+import DoctorService from '../../services/DoctorService';
+import PersonnelService from '../../services/PersonnelService';
+
+import {
+    putDoctorData
+} from './actions';
+
+export function* fetchNurseData() {
+    const { payload } = yield take(FETCH_DOCTOR_DATA);
+    const { data } = yield call(DoctorService.fetchDoctorData, payload);
+    yield put(putDoctorData(data));
+}
