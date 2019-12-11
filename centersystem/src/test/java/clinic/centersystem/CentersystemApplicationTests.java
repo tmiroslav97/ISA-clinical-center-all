1b91a9ec439d7476b5563792e29e657ed603912a
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-11 18:33:18 +0100

    Adding assert dependency

--- centersystem/src/test/java/clinic/centersystem/CentersystemApplicationTests.java
+++ centersystem/src/test/java/clinic/centersystem/CentersystemApplicationTests.java
@@ -2,6 +2,8 @@ package clinic.centersystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class CentersystemApplicationTests {
