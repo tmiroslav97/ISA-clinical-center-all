f7ad296be6d41e7ce1fa1ae5c5178d62f052f003
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-11 17:50:30 +0100

    Adding events to doctor calendar

--- client/src/components/WorkCalendar.js
+++ client/src/components/WorkCalendar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Spinner, Row, Col } from 'react-bootstrap';
+import { Container, Spinner, Row, Col, Table, Button } from 'react-bootstrap';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
@@ -12,7 +12,12 @@ const localizer = momentLocalizer(moment)
 const WorkCalendar = ({ personnelId }) => {
     const dispatch = useDispatch();
     const calendar = useSelector(calendarDataSelector);
-
+    const [event, setEvent] = useState({
+        title: '',
+        type: '',
+        start: '',
+        end: ''
+    });
     useEffect(() => {
         if (personnelId != null) {
             dispatch(
@@ -22,6 +27,10 @@ const WorkCalendar = ({ personnelId }) => {
             );
         }
     }, [personnelId]);
+
+    const handleCalendarClick = (calEvent) => {
+        setEvent(calEvent);
+    };
 
     return (
         <Container>
@@ -39,11 +48,50 @@ const WorkCalendar = ({ personnelId }) => {
                         startAccessor="start"
                         endAccessor="end"
                         style={{ height: 600 }}
+                        onSelectEvent={(calEvent) => {
+                            handleCalendarClick(calEvent);
+                        }}
                     />
                 </Col>
             </Row>
-
-
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <h2 className="border-bottom">Selected event: </h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{ span: 10, offset: 1 }} xs={12}>
+                    <Table responsive>
+                        <tbody>
+                            <tr>
+                                <th>Title</th>
+                                <td align="right">{event.title}</td>
+                            </tr>
+                            <tr>
+                                <th>Type</th>
+                                <td align="right">{event.type}</td>
+                            </tr>
+                            <tr>
+                                <th>Start date</th>
+                                <td align="right">{event.start}</td>
+                            </tr>
+                            <tr>
+                                <th>End date</th>
+                                <td align="right">{event.end}</td>
+                            </tr>
+                            {
+                                event.type === 'Appointment' &&
+                                <tr>
+                                    <th>Start appointment</th>
+                                    <td colSpan="2" align="right">
+                                        <Button>Start</Button>
+                                    </td>
+                                </tr>
+                            }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
         </Container>
     );
 }
