b7c5a0a1e3bace3d282caeee3733614adbd641d1
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-11 17:18:27 +0100

    Adding saga function for doctor calendar

--- client/src/store/doctor/saga.js
+++ client/src/store/doctor/saga.js
@@ -1,15 +1,23 @@
 import { take, put, call } from 'redux-saga/effects';
 
 import {
-    FETCH_DOCTOR_DATA
+    FETCH_DOCTOR_DATA,
+    FETCH_DOCTOR_CALENDAR
 } from './constants';
 
 import {
-    putDoctorData
+    putDoctorData,
+    putDoctorCalendarData
 } from './actions';
 
 import DoctorService from '../../services/DoctorService';
 import PersonnelService from '../../services/PersonnelService';
+
+export function* fetchDoctorCalendar(){
+    const { payload } = yield take(FETCH_DOCTOR_CALENDAR);
+    const { calendar } = yield call(PersonnelService.fetchCalendar, payload);
+    yield put(putDoctorCalendarData(calendar));
+}
 
 export function* fetchDoctorData() {
     const { payload } = yield take(FETCH_DOCTOR_DATA);
