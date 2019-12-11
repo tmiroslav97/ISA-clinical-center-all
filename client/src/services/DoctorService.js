011121b4af8f01bef3db90ba1919677aa8fee58e
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-11 13:40:51 +0100

    Adding service for doctor and implement service for fethcing doctor data

---
+++ client/src/services/DoctorService.js
@@ -0,0 +1,23 @@
+import HttpClient from './HttpBaseClient';
+import { format } from 'util';
+
+const FINALPOINTS = {
+    FETCH_DOCTOR_DATA: '/doctor/%s',
+};
+
+class DoctorService extends HttpClient {
+    fetchDoctorData = async payload => {
+        try {
+            const { data } = await this.getApiClient().get(
+                format(FINALPOINTS.FETCH_DOCTOR_DATA, payload.doctorId)
+            );
+
+            return { data };
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
+
+}
+
+export default new DoctorService();
