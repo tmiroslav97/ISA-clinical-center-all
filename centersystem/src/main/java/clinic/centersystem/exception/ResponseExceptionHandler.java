0822f91e658a9e5b66cab0f5cebef686f46c1061
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-13 13:01:22 +0100

    Adding exception handler for specific exceptions

---
+++ centersystem/src/main/java/clinic/centersystem/exception/ResponseExceptionHandler.java
@@ -0,0 +1,38 @@
+package clinic.centersystem.exception;
+
+import clinic.centersystem.model.RegistrationRequirement;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {UserExistsException.class})
+    protected ResponseEntity<Object> handleUserExistsException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "User already exists";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    protected ResponseEntity<Object> handleUserNotFoundException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "User not found";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = {CCANotPredefinedException.class})
+    protected ResponseEntity<Object> handleCCANotPredefinedException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Clinic center admin is not predefined admin";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {RegistrationRequirementNotFoundException.class})
+    protected ResponseEntity<Object> handleRegistrationRequirementNotFoundException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Registration requirement not found";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
