2ebaf20f6da88e93cc46e1d5cbeeb587350773b7
tmiroslav97 <tomic.miroslav97@gmail.com>
2019-12-13 13:43:13 +0100

    Adding exception handler for username not found exception

--- centersystem/src/main/java/clinic/centersystem/exception/ResponseExceptionHandler.java
+++ centersystem/src/main/java/clinic/centersystem/exception/ResponseExceptionHandler.java
@@ -4,6 +4,7 @@ import clinic.centersystem.model.RegistrationRequirement;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -36,10 +37,19 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
+    @ExceptionHandler(value = {UsernameNotFoundException.class})
+    protected ResponseEntity<Object> handleUserNameNotFoundException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "User by email can't be found";
+        System.out.println(bodyOfResponse);
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
     @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
         String bodyOfResponse = "Service unavailable";
         System.out.println("Exception in app occurred");
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE, request);
     }
+
+
 }
