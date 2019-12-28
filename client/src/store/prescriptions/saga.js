a8ae914fdb74a609f5bd7472080a96e421f638fa
Miroslav <tomic.miroslav97@gmail.com>
2019-12-28 19:51:01 +0100

    Added prescription store

---
+++ client/src/store/prescriptions/saga.js
@@ -0,0 +1,33 @@
+import { take, put, call } from 'redux-saga/effects';
+
+import {
+    FETCH_PRESCRIPTIONS,
+    REWRITE_PRESCRIPTION
+} from './constants';
+
+import NurseService from '../../services/NurseService';
+
+import {
+    putIsFetchPrescriptions,
+    putPrescriptionsData
+} from './actions';
+
+
+export function* rewritePrescription() {
+    const { payload } = yield take(REWRITE_PRESCRIPTION);
+    const { data } = yield call(NurseService.rewriteRecepie, payload);
+    put(putIsFetchPrescriptions(false));
+    const { recepies } = yield call(NurseService.fetchRecepies, {});
+    yield put(putRecepiesData(recepies));
+    put(putIsFetchPrescriptions(true));
+
+}
+
+export function* fetchPrescriptions() {
+    const { payload } = yield take(FETCH_PRESCRIPTIONS);
+    put(putIsFetchPrescriptions(false));
+    const { recepies } = yield call(NurseService.fetchRecepies, payload);
+    yield put(putRecepiesData(recepies));
+    put(putIsFetchPrescriptions(true));
+}
+
