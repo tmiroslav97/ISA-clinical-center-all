c2370b6ee1696c7956a7b2dcb09f89808e7da216
Miroslav <tomic.miroslav97@gmail.com>
2020-01-18 16:26:04 +0100

    Added repository for clinic rooms

---
+++ centersystem/src/main/java/clinic/centersystem/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package clinic.centersystem.repository;
+
+import clinic.centersystem.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
