f1f4622b89241c74f4f9ff831fc5437661815e00
Miroslav <tomic.miroslav97@gmail.com>
2020-01-18 17:03:16 +0100

    Added repository for fetching rooms by clinic id

--- centersystem/src/main/java/clinic/centersystem/repository/RoomRepository.java
+++ centersystem/src/main/java/clinic/centersystem/repository/RoomRepository.java
@@ -3,5 +3,8 @@ package clinic.centersystem.repository;
 import clinic.centersystem.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findByClinic(Long id);
 }
