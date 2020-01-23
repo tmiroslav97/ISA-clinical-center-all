25961d131c384a899cce519d255f0e19dc184524
Miroslav <tomic.miroslav97@gmail.com>
2020-01-23 17:41:57 +0100

    Added methods for surgery requirement

--- centersystem/src/main/java/clinic/centersystem/service/SurgeryRequirementServiceImpl.java
+++ centersystem/src/main/java/clinic/centersystem/service/SurgeryRequirementServiceImpl.java
@@ -1,10 +1,16 @@
 package clinic.centersystem.service;
 
+import clinic.centersystem.dto.response.RoomResponseDTO;
+import clinic.centersystem.dto.response.SurgeryRequirementResponseDTO;
+import clinic.centersystem.model.Room;
 import clinic.centersystem.model.Surgery;
 import clinic.centersystem.model.SurgeryRequirement;
 import clinic.centersystem.repository.SurgeryRequirementRepository;
 import clinic.centersystem.service.intf.SurgeryRequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,5 +29,17 @@ public class SurgeryRequirementServiceImpl implements SurgeryRequirementService 
     @Override
     public List<SurgeryRequirement> findAll() {
         return surgeryRequirementRepository.findAll();
+    }
+
+    @Override
+    public SurgeryRequirementResponseDTO findByClinicId(Long clinicId, Integer pageCnt) {
+        Pageable pageable = PageRequest.of(pageCnt, 10);
+        Page<SurgeryRequirement> surgeryRequirements = surgeryRequirementRepository.findByClinicId(clinicId, pageable);
+        SurgeryRequirementResponseDTO surgeryRequirementResponseDTO = new SurgeryRequirementResponseDTO();
+
+        surgeryRequirementResponseDTO.setSurgeryRequirements(surgeryRequirements.getContent());
+        surgeryRequirementResponseDTO.setPageCount(surgeryRequirements.getTotalPages());
+
+        return surgeryRequirementResponseDTO;
     }
 }
--- centersystem/src/main/java/clinic/centersystem/service/intf/SurgeryRequirementService.java
+++ centersystem/src/main/java/clinic/centersystem/service/intf/SurgeryRequirementService.java
@@ -1,5 +1,6 @@
 package clinic.centersystem.service.intf;
 
+import clinic.centersystem.dto.response.SurgeryRequirementResponseDTO;
 import clinic.centersystem.model.SurgeryRequirement;
 
 import java.util.List;
@@ -7,5 +8,9 @@ import java.util.List;
 public interface SurgeryRequirementService {
 
     SurgeryRequirement findById(Long id);
+
     List<SurgeryRequirement> findAll();
+
+    SurgeryRequirementResponseDTO findByClinicId(Long clinicId, Integer pageCnt);
+
 }
