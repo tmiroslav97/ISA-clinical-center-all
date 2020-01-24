878eda2c65e12908bd25381cd4dcdc08ff563810
Miroslav <tomic.miroslav97@gmail.com>
2020-01-24 19:19:17 +0100

    Added view for surgery requirements

--- client/src/store/sur-req/saga.js
+++ client/src/store/sur-req/saga.js
@@ -4,6 +4,7 @@ import {
     FETCH_SUR_REQ_DATA
 } from './constants';
 
+import SurgeryRequirementService from '../../services/SurgeryRequirementService';
 
 import {
     putIsFetchSurReqData,
@@ -12,4 +13,11 @@ import {
 } from './actions';
 
 
-
+export function* fetchRoomsData() {
+    const { payload } = yield take(FETCH_SUR_REQ_DATA);
+    yield put(putIsFetchSurReqData(false));
+    const { data } = yield call(SurgeryRequirementService.fetchSurReqData, payload);
+    yield put(putSurReqData(data.surgeryRequirements));
+    yield put(putSurReqPageCount(data.pageCount));
+    yield put(putIsFetchSurReqData(true));
+}
