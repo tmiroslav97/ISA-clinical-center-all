e03bc47253856b79bc6c9f801f60111f035316c8
Nevena <nvndjukin97@gmail.com>
2020-02-05 15:34:27 +0100

    Fix conflict

--- client/src/store/appointments/selectors.js
+++ client/src/store/appointments/selectors.js
@@ -1,11 +1,7 @@
 const reducer = 'appointmentReducer';
 
 export const appointmentTypeSelector = state => state[reducer].appointmentTypes;
-<<<<<<< HEAD
-export const isFetchAppointmentTypeSelector = state => state[reducer].isFetchAppointmentTypes;
-=======
 export const isFetchAppointmentTypeSelector = state => state[reducer].isFetchAppointmentTypes;
 export const appIdSelector = state => state[reducer].id;
 export const appointmentSelector = state => state[reducer].appointment;
 export const isFetchAppointmentSelector = state => state[reducer].isFetchAppointment;
->>>>>>> master
