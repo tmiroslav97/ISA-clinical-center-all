37f7c50e062a2d4b4673e053f2c58a2389d8a2a4
Miroslav <tomic.miroslav97@gmail.com>
2020-02-08 14:29:57 +0100

    Changed configuration for deployment

---
+++ centersystem/client/src/services/ClinicService.js
@@ -0,0 +1,83 @@
+import HttpClient from './HttpBaseClient';
+import { history } from '../index';
+import { format } from 'util';
+
+const FINALPOINTS = {
+    REG_CLINIC: '/clinic/reg-clinic',
+    FETCH_CLINICS: '/clinic/clinics',
+    REG_CLINIC_ADMIN: '/adm-cli/reg-clinic-admin',
+    FETCH_CLINIC_PAGINATION_DATA: '/clinic/all/%s'
+};
+
+class ClinicService extends HttpClient {
+
+    regClinic = async payload => {
+        try {
+            const { data } = await this.getApiClient().post(
+                FINALPOINTS.REG_CLINIC,
+                payload
+            );
+
+            return { data };
+        } catch (error) {
+            return error.response;
+        }
+    };
+
+    fetchClinicsData = async payload => {
+        try {
+            const { data } = await this.getApiClient().get(
+                FINALPOINTS.FETCH_CLINICS
+            );
+
+            const clinics = data;
+
+            return { clinics };
+        } catch (error) {
+            return error.response;
+        }
+    };
+
+    regClinicAdmin = async payload => {
+        try {
+            const { data } = await this.getApiClient().post(
+                FINALPOINTS.REG_CLINIC_ADMIN,
+                payload
+            );
+
+            return { data };
+        } catch (error) {
+            return error.response;
+        }
+    };
+
+    searchClinic = async payload => {
+        try {
+            const { data } = await this.getApiClient().post(
+                FINALPOINTS.SEARCH_CLINIC,
+                payload
+            );
+
+            return { data };
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+    
+    fetchClinicPaginationData = async payload => {
+        try {
+            const { data } = await this.getApiClient().get(
+                format(FINALPOINTS.FETCH_CLINIC_PAGINATION_DATA, payload.pageCnt)
+            );
+            
+            const clinicPag = data;
+            return { clinicPag };
+        } catch (error) {
+            return error.response;
+        }
+    };
+
+
+}
+
+export default new ClinicService();
